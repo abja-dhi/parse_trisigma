81ce66718e469736034ccbd8faad3ed6e37749fa
abja-dhi <abja@dhigroup.com>
2024-05-10 13:54:51 -0400

    GPS Parser Updated

--- parse_trisigma/parser.py
+++ parse_trisigma/parser.py
@@ -1,9 +1,7 @@
 import os
 import camelot
 import pandas as pd
-import locale
-locale.setlocale(locale.LC_ALL, "nl_NL")
-import datetime
+from datetime import datetime, timedelta
 import time
 from string import ascii_uppercase as apc
 import geopandas as gpd
@@ -12,9 +10,11 @@ from shapely.geometry import Point
 import matplotlib.pyplot as plt
 from .utils import DMS2DD, str2latlon, get_number, set_date, m3, get_lat, get_lon, find_area
 from myUtils.mkch import mkch
+import locale
 
-class Parser:
+class Parser_report:
     def __init__(self, out_path, reports_path) -> None:
+        locale.setlocale(locale.LC_ALL, "nl_NL")
         self.out_path = out_path
         self.reports_path = reports_path
         mkch(out_path)
@@ -35,7 +35,7 @@ class Parser:
         self.datetimes = []
         for d in self.dates:
             d = d.replace("Daily production Report - ", "").replace("DPR", "").replace(".pdf", "").replace("KA3", "").replace(" ", "").capitalize().replace("desember", "december")
-            self.datetimes.append(datetime.datetime.strptime(d, "%d%B%Y").strftime("%Y-%m-%d"))
+            self.datetimes.append(datetime.strptime(d, "%d%B%Y").strftime("%Y-%m-%d"))
 
         self.df_tot = pd.DataFrame()
         for i in range(len(self.fnames)):
@@ -93,3 +93,138 @@ class Parser:
         return df
     
     #def fill_missings(self):
+
+
+class Parser_GPS:
+    import locale
+    locale.setlocale(locale.LC_TIME, 'en_US.UTF-8')
+    def __init__(self, dir) -> None:
+        self.dir = dir
+        self.files = [os.path.join(self.dir, f) for f in os.listdir(self.dir)]
+
+    def parse(self, filename=None):
+        """Parses GPS logs from Trisigma
+        if filename is defined, the function only parses that specific file,
+        otherwise it parses all the files inside the root directory of the class
+        
+        Examples:
+        >>> parser_object.parse()
+        >>> parser_object.parse(filename=filename)
+        """
+
+        if filename is None:
+            for f in self.files:
+                self._parse(f)
+        else:
+            self._parse(os.path.join(self.dir, filename))
+
+    def _parse(self, filename):
+        lines = open(filename, 'r').readlines()
+        df = pd.DataFrame(columns=["Time", "East", "North", "Lon", "Lat", "Course", "Speed", "Draft", "Distance"])
+        for l in lines:
+            data = self._parse_line(l)
+            print(data["Time"])
+            df = pd.concat([df, pd.DataFrame([data])], ignore_index=True)
+        fname = os.path.basename(filename)
+        df.to_csv(filename.replace(fname, "Processed_"+fname), index=False)    
+        return df
+            
+
+    def _parse_line(self, line):
+        line = line.replace("#NAME?", "").strip()
+        elems = line.split("=")
+        funcs = {"Time": self._parse_time, "East": self._parse_east_north, "North": self._parse_east_north,
+                 "Lon": self._parse_lon_lat, "Lat": self._parse_lon_lat, "Course": self._parse_course,
+                 "Speed": self._parse_speed, "Draft": self._parse_draft, "Distance": self._parse_distance}
+        data = {key: np.nan for key in funcs.keys()}
+        for elem in elems:
+            for key in funcs.keys():
+                if key.lower() in elem.lower():
+                    data[key] = funcs[key](elem)
+                    break
+                
+        return data
+            
+
+    def _parse_time(self, elem):
+        if "$$$$" in elem:
+            return np.nan
+        else:
+            elem = elem.replace("\t", " ")
+            info = elem.split(",")
+            data = info[-1]
+            if data == "":
+                data = info[-2]
+            try:
+                data = datetime.strptime(data.strip(), "%A %d %B %Y %H:%M:%S")
+            except:
+                data = datetime.strptime(data.strip(), '%d %B %Y %H:%M:%S')
+            return data
+
+
+    def _parse_east_north(self, elem):
+        if "$$$$" in elem:
+            return np.nan
+        else:
+            info = elem.lower().replace("east", "").replace("north", "").replace(" ", "").replace("mi", "").replace("m", "").replace(",","").replace("ing", "").replace("(", "").replace(")", "").replace("naut", "").replace("\t", "")
+            data = float(info)
+            # Convert mi(naut) to m
+            if data < 10000:
+                data = data * 1852
+            return data
+
+    def _parse_lon_lat(self, elem):
+        if "$$$$" in elem:
+            return np.nan
+        else:
+            info = elem.lower().replace("latitude", "").replace("longitude", "").replace("lat", "").replace("long", "").replace("lon", "").replace("s", "").replace("e", "").replace(",", "").replace("â", "").replace("\t", "").replace("ï¿½", "°")
+            deg = float(info.split("°")[0])
+            minute = float(info.split("°")[1].split("'")[0])
+            if '"' not in info:
+                second = 0
+            else:
+                second = float(info.split("°")[1].split("'")[1].split('"')[0])
+            data = deg + minute / 60 + second / 3600
+            if "S" in elem:
+                data = -1 * data
+            return data
+
+    def _parse_speed(self, elem):
+        if "$$$$" in elem:
+            return np.nan
+        else:
+            info = elem.lower().replace("speed", "").replace("kts", "").replace(",", "").replace("\t", "")
+            # Convert knot to m/s
+            data = float(info) / 1.944
+            return data
+
+    def _parse_course(self, elem):
+        if "$$$$" in elem:
+            return np.nan
+        else:
+            if "###" in elem:
+                return np.nan
+            else:
+                info = elem.lower().replace("course", "").replace(",", "").replace("â", "").replace("�", "").replace("\t", "").replace("ï¿½", "°")
+                deg = float(info.split("°")[0])
+                if "'" not in info:
+                    minute = 0
+                else:
+                    minute = float(info.split("°")[1].split("'")[0])
+                if '"' not in info:
+                    second = 0
+                else:
+                    second = float(info.split("°")[1].split("'")[1].split('"')[0])
+                data = deg + minute / 60 + second / 3600
+                return data
+
+    def _parse_draft(self, elem):
+        if "$$$$" in elem:
+            return np.nan
+        else:
+            info = elem.lower().replace("draft", "").replace(",", "").replace("m", "").replace(" ", "").replace("\t", "")
+            data = float(info)
+            return data
+
+    def _parse_distance(self, elem):
+        return np.nan
